6e416f87472ae044cc1e2743cf9e1098ba995977
Гарас Сергей Ярославович <sergeygaras2002@gmail.com>
2023-06-27 17:01:43 +0300

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -8,8 +10,9 @@ import 'package:webview_flutter_android/webview_flutter_android.dart';
 import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 import 'package:webview_flutter_platform_interface/webview_flutter_platform_interface.dart';
 
+
 void main() {
-  runApp(const TestPostmessageApp());
+  runApp( TestPostmessageApp());
 }
 
 
@@ -39,90 +42,76 @@ class TestPostmessageAppState extends State<TestPostmessageApp>{
 
   Future<bool> initStateAsync() async {
 
-    late final PlatformWebViewControllerCreationParams params;
 
-    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
-      params = WebKitWebViewControllerCreationParams(
-        allowsInlineMediaPlayback: true,
-        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{}
-      );
-    } else {
-      params = const PlatformWebViewControllerCreationParams();
-    }
+    var controller = WebViewController();
 
-    final WebViewController controller2 = WebViewController.fromPlatformCreationParams(params);
+    await controller.setJavaScriptMode(JavaScriptMode.unrestricted);
 
-    await controller2.setJavaScriptMode(JavaScriptMode.unrestricted);
+    await (controller.platform as AndroidWebViewController)
+        .addJavaScriptChannel(JavaScriptChannelParams(
+        name: 'GERC',
+        onMessageReceived: (JavaScriptMessage message) {
+          print('GERC : ${message.message}');
 
-    if (controller2.platform is AndroidWebViewController) {
+          var data = json.decode(message.message);
 
-      await AndroidWebViewController.enableDebugging(true);
-      await (controller2.platform as AndroidWebViewController).setMediaPlaybackRequiresUserGesture(false);
 
-      await (controller2.platform as AndroidWebViewController).addJavaScriptChannel(
-          JavaScriptChannelParams(name: 'GERC', onMessageReceived: (JavaScriptMessage message) {
-            print('GERC : ${message.message}');
-          })
-      );
-      await (controller2.platform as AndroidWebViewController).runJavaScript("""
-              window.postMessage = function(data) {
-                         window.GERC.postMessage(JSON.stringify(data));
-                     };
-           """);
-      await (controller2.platform as AndroidWebViewController).loadRequest(LoadRequestParams(uri: Uri.parse(widget.paymentUrl)));
-    }
+          if (data.containsKey('type')) {
+            if (data['type'] == "payment-status") {
+              print("object");
+            }
+          }
+        }));
 
-    await controller2.setNavigationDelegate(NavigationDelegate(
+
+    await controller.loadRequest(Uri.parse(
+        widget.paymentUrl));
+
+    await controller
+        .setNavigationDelegate(NavigationDelegate(
         onPageStarted: (url) async {
-          await controller2.runJavaScriptReturningResult("""
-             window.postMessage = function(data) {
+      await controller.runJavaScript("""
+             window.parent.postMessage = function(data) {
                         window.GERC.postMessage(JSON.stringify(data));
                     };
           """);
-        },
-        onPageFinished: (url) async {
-          await controller2.runJavaScriptReturningResult("""
-             window.postMessage = function(data) {
+    }, onPageFinished: (url) async {
+      await controller.runJavaScript("""
+             window.parent.postMessage = function(data) {
                         window.GERC.postMessage(JSON.stringify(data));
                     };
           """);
-        },
-        onNavigationRequest: (NavigationRequest request) async {
+    }));
 
-          await controller2.runJavaScriptReturningResult("""
-             window.postMessage = function(data) {
-                        window.GERC.postMessage(JSON.stringify(data));
-                    };
-          """);
 
-          return NavigationDecision.navigate;
-        }
-    ));
-
-    await controller2.loadRequest(Uri.parse(widget.paymentUrl)).then((_) async => {
-      await controller2.runJavaScriptReturningResult("""
-             window.postMessage = function(data) {
+    await controller
+        .loadRequest(Uri.parse(widget.paymentUrl))
+        .then((_) async => {
+      await controller.runJavaScript("""
+             window.parent.postMessage = function(data) {
                         window.GERC.postMessage(JSON.stringify(data));
                     };
           """)
     });
 
-    await controller2.runJavaScriptReturningResult("""
-             window.postMessage = function(data) {
+    await controller.runJavaScriptReturningResult("""
+             window.parent.postMessage = function(data) {
                         window.GERC.postMessage(JSON.stringify(data));
                     };
-          """).then((_) async => await Future.delayed(Duration(seconds: 1),(){
-        controller2.loadRequest(Uri.parse(widget.paymentUrl));
+          """).then((_) async => await Future.delayed(
+        const Duration(seconds: 1), () {
+      controller.loadRequest(Uri.parse(widget.paymentUrl));
     }));
 
-    _controller = await controller2;
+    _controller = controller;
 
     return true;
   }
 
   @override
   Widget build(BuildContext context) {
-      return MaterialApp(
+
+    return MaterialApp(
         home: SafeArea(
             child: Scaffold(
                 body: FutureBuilder<bool>(
@@ -140,3 +129,85 @@ class TestPostmessageAppState extends State<TestPostmessageApp>{
   }
 
 }
+//
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(title: Text('WebView with postMessage Listener')),
+//         body: WebViewWithPostMessageListener(),
+//       ),
+//     );
+//   }
+// }
+//
+// class WebViewWithPostMessageListener extends StatefulWidget {
+//   @override
+//   _WebViewWithPostMessageListenerState createState() =>
+//       _WebViewWithPostMessageListenerState();
+// }
+//
+// class _WebViewWithPostMessageListenerState
+//     extends State<WebViewWithPostMessageListener> {
+//   final webViewPlugin = FlutterWebviewPlugin();
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//
+//     webViewPlugin.onStateChanged.listen((WebViewStateChanged state) {
+//       if (state.type == WebViewState.finishLoad) {
+//
+//         webViewPlugin.evalJavascript("""
+//                  window.parent.postMessage = function(data) {
+//                             window.GERC.postMessage(JSON.stringify(data));
+//                         };
+//               """);
+//         webViewPlugin.evalJavascript('''
+//           window.addEventListener("message", (event) => {
+//             window.parent.postMessage(JSON.stringify(event.data));
+//           });
+//         ''');
+//       }
+//     });
+//
+//     webViewPlugin.evalJavascript("""
+//                  window.parent.postMessage = function(data) {
+//                             window.GERC.postMessage(JSON.stringify(data));
+//                         };
+//               """);
+//
+//     // webViewPlugin.onJavascriptChannelReceive.listen((JavascriptChannelMessage message) {
+//     //   print('Received message from WebView: ${message.message}');
+//     // });
+//   }
+//
+//   @override
+//   void dispose() {
+//     webViewPlugin.dispose();
+//     super.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return WebviewScaffold(
+//       url: 'https://fc.gerc.ua:8443/api_test/postMessage.php',
+//       headers: <String, String>{
+//         'Access-Control-Allow-Origin': '*',
+//         'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+//         'Access-Control-Allow-Credentials': 'true',
+//         'Content-Type': 'type="text/javascript'
+//       },
+//       javascriptChannels: {
+//         JavascriptChannel(
+//           name: 'parent',
+//           onMessageReceived: (JavascriptMessage message) {
+//             print('Received message from WebView: ${message.message}');
+//           },
+//         ),
+//       },
+//     );
+//   }
+// }
